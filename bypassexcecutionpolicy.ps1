32d1ddfe221e6f1132bafa8de000501f714647aa
Robert John P. Canare <33864725+robertcanare@users.noreply.github.com>
2018-06-21 21:07:11 +0800

    Create bypassexcecutionpolicy.ps1

---
+++ bypassexcecutionpolicy.ps1
@@ -0,0 +1,3 @@
+﻿#bypassing execution policy
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"C:\directory\to\script\task.ps1`"" -PassThru -Verb RunAs }
