48348c790cdd7d643bf531ca09de17632188a1d5
Robert John P. Canare <33864725+robertcanare@users.noreply.github.com>
2018-06-21 21:05:20 +0800

    Create tasksheduler.ps1

---
+++ tasksheduler.ps1
@@ -0,0 +1,6 @@
+﻿#add scheduler
+
+$action = New-ScheduledTaskAction -Execute "C:\directory\to\file.exe" -Argument "-f" -WorkingDirectory "C:\directory\to\"
+$trigger = New-ScheduledTaskTrigger -Daily -at 10:20am #time
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "sublime" -Description "test to launch" -RunLevel Highest -Force
